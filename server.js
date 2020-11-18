719e0482d4409ec6997076fffbda248ce6e9c979
Diego Queres <diego.queres@stoom.com.br>
2020-11-18 19:54:40 -0300

    Restructuring the project

--- server.js
+++ server.js
@@ -2,25 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
-//iniciando o app
+// Iniciando o app
 const app = express();
 
-//iniciando o DB
+// Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-//primeira rota
-app.get("/", (req, res) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://www.ig.com.br'
-    });
-
-
-    res.send("Hello Rocketseat III");
-})
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
